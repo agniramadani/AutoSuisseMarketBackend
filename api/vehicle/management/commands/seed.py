7a58f28331eab8d5c9e1eb240b1c7cc776236d59
Agni <agni.ramadani@gmail.com>
2024-10-12 16:18:34 +0200

    Refine seeders.

--- api/vehicle/management/commands/seed.py
+++ api/vehicle/management/commands/seed.py
@@ -1,12 +1,12 @@
-import os
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 from vehicle.models import Vehicle, VehicleImage
+from django.contrib.auth.models import User
 from django.core.files import File
+import os
+
 
 class Command(BaseCommand):
     help = 'Seed the database with fake users, vehicles, and images'
-
     # Change to the script's directory
     os.chdir(os.path.dirname(os.path.abspath(__file__)))    
 
@@ -29,21 +29,17 @@ class Command(BaseCommand):
             first_name='Nicolas', last_name='Schneider', email='nicolas.schneider@fake.ch'
         )
 
-        # Seed Vehicles
+        # Create Vehicles
         vehicles_data = [
             {'owner': user1, 'make': 'TOYOTA', 'model': 'CAMRY', 'year': 2015, 'price': 12000, 'mileage': 80000, 'color': 'Red', 'fuel_type': 'Petrol', 'transmission': 'Automatic'},
             {'owner': user4, 'make': 'TOYOTA', 'model': 'COROLLA', 'year': 2020, 'price': 18000, 'mileage': 30000, 'color': 'Blue', 'fuel_type': 'Hybrid', 'transmission': 'Manual'},
             {'owner': user2, 'make': 'HONDA', 'model': 'CIVIC', 'year': 2018, 'price': 14000, 'mileage': 60000, 'color': 'Blue', 'fuel_type': 'Petrol', 'transmission': 'Manual'},
             {'owner': user3, 'make': 'TESLA', 'model': 'MODEL S', 'year': 2020, 'price': 60000, 'mileage': 20000, 'color': 'Black', 'fuel_type': 'Electric', 'transmission': 'Automatic'},
-            
             {'owner': user3, 'make': 'MERCEDES-BENZ', 'model': 'C-CLASS', 'year': 2018, 'price': 25000, 'mileage': 45000, 'color': 'Black', 'fuel_type': 'Diesel', 'transmission': 'Automatic'},
             {'owner': user1, 'make': 'MERCEDES-BENZ', 'model': 'E-CLASS', 'year': 2021, 'price': 45000, 'mileage': 15000, 'color': 'White', 'fuel_type': 'Petrol', 'transmission': 'Automatic'},
-            
             {'owner': user2, 'make': 'BMW', 'model': '3 SERIES', 'year': 2019, 'price': 35000, 'mileage': 38000, 'color': 'Grey', 'fuel_type': 'Petrol', 'transmission': 'Automatic'},
-            
             {'owner': user4, 'make': 'VOLKSWAGEN', 'model': 'GOLF 7', 'year': 2017, 'price': 20000, 'mileage': 50000, 'color': 'White', 'fuel_type': 'Petrol', 'transmission': 'Manual'},
             {'owner': user3, 'make': 'VOLKSWAGEN', 'model': 'GOLF 8', 'year': 2021, 'price': 32000, 'mileage': 15000, 'color': 'Blue', 'fuel_type': 'Hybrid', 'transmission': 'Automatic'},
-            
             {'owner': user4, 'make': 'AUDI', 'model': 'A4', 'year': 2016, 'price': 28000, 'mileage': 60000, 'color': 'Silver', 'fuel_type': 'Diesel', 'transmission': 'Manual'},
         ]
 
@@ -51,7 +47,7 @@ class Command(BaseCommand):
             vehicle = Vehicle.objects.create(**data)
             self.stdout.write(f"Created {vehicle}")
 
-            # Add images to vehicles from folder
+            # Add images to vehicles
             image_folder = f'./seed_cars/{data["make"]}/{data["model"]}'
             if os.path.exists(image_folder):
                 for img_file in os.listdir(image_folder):
